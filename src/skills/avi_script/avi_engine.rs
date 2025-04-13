59b5a468806d57b485bf8bac7083b4466603c437
Apoll011 <tiagorobotik@gmail.com>
2025-04-13 16:45:24 -0100

    added engine helpers

---
+++ src/skills/avi_script/avi_engine.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+use rhai::{Array, Engine, Scope};
+use crate::skills::avi_script::engine::create_avi_script_engine;
+use crate::skills::avi_script::modules::register_modules;
+
+pub fn get_avi_engine() -> Result<Engine, Box<dyn Error>>
+{
+    let engine = create_avi_script_engine(register_modules);
+
+    Ok(engine?)
+}
+
+pub fn run_avi(engine: Engine, path: String) -> Result<(), Box<dyn Error>>{
+    let mut scope = Scope::new();
+
+    let supported_languages: Array = vec!["pt".into(), "en".into()];
+
+    scope.push_constant("SKILL_NAME", "test")
+        .push_constant("CURRENT_LANGUAGE", "pt")
+        .push_constant("SUPPORTED_LANGUAGES", supported_languages);
+
+    engine.run_file_with_scope(&mut scope, path.into())?;
+
+    Ok(())
+}
