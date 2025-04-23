4d34c4cb1fd6cdc69911c70f82f3a9c37690de13
Apoll011 <tiagorobotik@gmail.com>
2025-04-23 16:01:49 -0100

    Bump version to 0.1.3
    Signed-off-by: Apoll011 <tiagorobotik@gmail.com>

--- src/version.rs
+++ src/version.rs
@@ -1,3 +1,3 @@
-pub const VERSION: &str = "0.1.2";
+pub const VERSION: &str = "0.1.3";
 pub const AUTHOR: &str = "Tiago Rodrigo dos Reis Inês";
-pub const BUILD_DATE: &str = "2025-04-22";
+pub const BUILD_DATE: &str = "2025-04-23";
